2e31b9b238c4facac3d51a1f5547075977b27141
Aakriti313 <dobleagueber313@gmail.com>
2024-03-12 19:49:53 +0100

    Updated archieve 2024_03_12_121345_create_bills_table #40

--- database/migrations/2024_03_12_121345_create_bills_table.php
+++ database/migrations/2024_03_12_121345_create_bills_table.php
@@ -16,6 +16,7 @@ class CreateBillsTable extends Migration
         Schema::create('bills', function (Blueprint $table) {
             $table->id();
             $table->foreignId("patients_id")->constrained();
+            $table->string('amount');
             $table->enum("status", ["paid", "unpaid"])->default("unpaid");
             $table->timestamps();
             $table->softDeletes();
