f402d0aea872e2c797b0d10f415ce41e613248d2
Omirzar1337 <omirzar@gmail.com>
2024-04-03 19:12:34 +0200

    Captcha integration for the Laravel #171 Changes contact submit btn, adds grecaptcha scripts and adds updatedCaptcha on livewire component (WiP)

--- app/Http/Livewire/Contactus.php
+++ app/Http/Livewire/Contactus.php
@@ -4,6 +4,9 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\contact;
+use Illuminate\Support\Facades\Http;
+
+
 class Contactus extends Component
 {
     public $name;
@@ -11,6 +14,19 @@ class Contactus extends Component
     public $subject;
     public $message;
     public $email;
+    public $captcha = 0;
+
+    public function updatedCaptcha($token)
+    {
+        $response = Http::post('https://www.google.com/recaptcha/api/siteverify?secret=' . env('CAPTCHA_SECRET_KEY') . '&response=' . $token);
+        $this->captcha = $response->json()['score'];
+dd($this->captcha);
+        if (!$this->captcha = .3) {
+            $this->add_to_contact();
+        } else {
+            return session()->flash('success', 'Google thinks you are a bot, please refresh and try again');
+        }
+    }
 
     public function add_to_contact()
     {
@@ -20,7 +36,7 @@ class Contactus extends Component
             'subject' => 'required',
             'message' => 'required',
             'email' => 'required|email',
-            ]);
+        ]);
 
         contact::create([
             'name'         => $this->name,
@@ -30,14 +46,14 @@ class Contactus extends Component
             'message'         => $this->message,
         ]);
 
-           //unset variables
-           $this->email="";
-           $this->name="";
-           $this->phone="";
-           $this->subject="";
-           $this->message="";
+        //unset variables
+        $this->email = "";
+        $this->name = "";
+        $this->phone = "";
+        $this->subject = "";
+        $this->message = "";
 
-           session()->flash('message', 'Message Submitted Successfully!.');
+        session()->flash('message', 'Message Submitted Successfully!.');
     }
     public function render()
     {
--- resources/views/livewire/contactus.blade.php
+++ resources/views/livewire/contactus.blade.php
@@ -11,47 +11,57 @@
                 <div class="row">
                     <div class="col-md-6">
                         <div class="form-group in_name">
-                            <input type="text" class="form-control" placeholder="Name" wire:model.lazy="name"
-                                required="required" style="color: cyan">
+                            <input type="text" class="form-control" placeholder="Name" wire:model.lazy="name" required="required" style="color: cyan">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group in_email">
-                            <input type="email" class="form-control" wire:model.lazy="email" placeholder="E-mail"
-                                required="required " style="color: cyan">
+                            <input type="email" class="form-control" wire:model.lazy="email" placeholder="E-mail" required="required " style="color: cyan">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group in_email">
-                            <input type="tel" class="form-control" id="phone" wire:model.lazy="phone"
-                                placeholder="Phone" required="required" style="color: cyan">
+                            <input type="tel" class="form-control" id="phone" wire:model.lazy="phone" placeholder="Phone" required="required" style="color: cyan">
                         </div>
                     </div>
                     <div class="col-md-6">
                         <div class="form-group in_email">
-                            <input type="text" class="form-control" id="subject" wire:model.lazy="subject"
-                                placeholder="Subject" required="required" style="color: cyan">
+                            <input type="text" class="form-control" id="subject" wire:model.lazy="subject" placeholder="Subject" required="required" style="color: cyan">
                         </div>
                     </div>
                     <div class="col-md-12">
                         <div class="form-group in_message">
-                            <textarea class="form-control" id="message"  rows="5" placeholder="Message"
-                                wire:model.lazy="message" required="required" style="color: cyan"></textarea>
-                        </div>
+                            <textarea class="form-control" id="message" rows="5" placeholder="Message" wire:model.lazy="message" required="required" style="color: cyan"></textarea>
+                            <!-- </div>
                         <div class="actions">
                             <input type="submit" value="Send Message"  name="submit" id="submitButton" class="btn small"
                                 title="Submit Your Message!">
+                        </div> -->
+
+                            <button id="submitButton"  type="submit" data-sitekey="{{env('CAPTCHA_SITE_KEY')}}" data-callback='handle' data-action='submit' class="g-recaptcha btn small">
+                                Send Message
+                            </button>
                         </div>
                     </div>
-                </div>
             </form>
             @if (session()->has('message'))
-                <script>
-                    alert(' {{ session('message') }}')
-
-                </script>
+            <script>
+                alert("{{ session('message')}}");
+            </script>
             @endif
         </div>
         <div id="googleMap" style="width:100%;height:450px;"></div>
     </div>
 </div>
+
+<script src="https://www.google.com/recaptcha/api.js?render={{env('CAPTCHA_SITE_KEY')}}"></script>
+<script>
+    function handle(e) {
+        grecaptcha.ready(function () {
+            grecaptcha.execute("{{env('CAPTCHA_SITE_KEY')}}", {action: 'submit'})
+                .then(function (token) {
+                    @this.set('captcha', token);
+                });
+        })
+    }
+</script>
