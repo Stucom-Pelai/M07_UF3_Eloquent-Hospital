ba03aa2197a9d84358ef545f7069bb5eac010bff
NabilHG <nabilhajjoune@gmail.com>
2024-04-16 23:25:41 +0200

    done feature endlessmedical api

--- app/Http/Controllers/AnalyzeController.php
+++ app/Http/Controllers/AnalyzeController.php
@@ -6,208 +6,70 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Http\Client\RequestException;
 
-
 class AnalyzeController extends Controller
 {
-    
-
+    /**
+     * Analyzes symptoms provided by the user and retrieves potential diseases based on those symptoms.
+     * Initializes a session with the API, updates features based on user input, and fetches the analysis results.
+     */
     public function analyzeSymptoms(Request $request)
     {
-
         $sessionId = '';
-        //url example: https://api.endlessmedical.com/v1/dx/Analyze?SessionID=GeLIy3MDIeoIPNZQ&NumberOfResults=5
-        // response: we want Diseases key
-        // {
-        //     "status": "ok",
-        //     "Diseases": [
-        //         {
-        //         "Otitis media": "1.0007887223203893"
-        //         },
-        //         {
-        //         "Asthma exacerbation without acute respiratory failure": "1.0001724999988861"
-        //         },
-        //         {
-        //         "Acute bronchitis": "0.9999628037654515"
-        //         },
-        //         {
-        //         "Chronic obstructive pulmonary disease (COPD)": "0.9986944761108576"
-        //         },
-        //         {
-        //         "Viral pharyngitis (etiology usually rhinovirus, coronavirus, adenovirus, parainfluenza)": "0.9940290911941558"
-        //         }
-        //     ],
-        //     "VariableImportances": [
-        //         {
-        //         "Otitis media": [
-        //             [
-        //             "EarPainRos",
-        //             "1.0"
-        //             ],
-        //             [
-        //             "Temp",
-        //             "0.03496318412371139"
-        //             ],
-        //             [
-        //             "OtoscopicErythema",
-        //             "0.012202859801766438"
-        //             ],
-        //             [
-        //             "HearingLossRos",
-        //             "0.012171508524770287"
-        //             ],
-        //             [
-        //             "SeverityCough",
-        //             "0.006954659763718865"
-        //             ]
-        //         ]
-        //         },
-        //         {
-        //         "Asthma exacerbation without acute respiratory failure": [
-        //             [
-        //             "FEV1toFVCRatio",
-        //             "1.0"
-        //             ],
-        //             [
-        //             "pCO2onABG",
-        //             "0.2828223011017039"
-        //             ],
-        //             [
-        //             "O2Sats",
-        //             "0.2711368130942004"
-        //             ],
-        //             [
-        //             "DyspneaProgressionSubjective",
-        //             "0.0884402923085123"
-        //             ],
-        //             [
-        //             "DyspneaSeveritySubjective",
-        //             "0.06588620532380231"
-        //             ]
-        //         ]
-        //         },
-        //         {
-        //         "Acute bronchitis": [
-        //             [
-        //             "SeverityCough",
-        //             "1.0"
-        //             ],
-        //             [
-        //             "Wheezing",
-        //             "0.2713446236073552"
-        //             ],
-        //             [
-        //             "Age",
-        //             "0.008639811907095153"
-        //             ],
-        //             [
-        //             "Temp",
-        //             "0.0037054023103729014"
-        //             ],
-        //             [
-        //             "O2Sats",
-        //             "0.0027111627655544437"
-        //             ]
-        //         ]
-        //         },
-        //         {
-        //         "Chronic obstructive pulmonary disease (COPD)": [
-        //             [
-        //             "FEV1toFVCRatio",
-        //             "1.0"
-        //             ],
-        //             [
-        //             "DyspneaProgressionSubjective",
-        //             "0.7819580513208088"
-        //             ],
-        //             [
-        //             "PMHXCOPD",
-        //             "0.4094562233662651"
-        //             ],
-        //             [
-        //             "DyspneaSeveritySubjective",
-        //             "0.08714633263942984"
-        //             ],
-        //             [
-        //             "RR",
-        //             "0.015447668459893942"
-        //             ]
-        //         ]
-        //         },
-        //         {
-        //         "Viral pharyngitis (etiology usually rhinovirus, coronavirus, adenovirus, parainfluenza)": [
-        //             [
-        //             "PCRHIVDNA",
-        //             "1.0"
-        //             ],
-        //             [
-        //             "PCRFlu",
-        //             "0.4839982969936215"
-        //             ],
-        //             [
-        //             "RapiStrepTest",
-        //             "0.3096239797372729"
-        //             ],
-        //             [
-        //             "PCRCovid",
-        //             "0.25163269735908705"
-        //             ],
-        //             [
-        //             "HIVWesternBlot",
-        //             "0.17790715897802004"
-        //             ]
-        //         ]
-        //         }
-        //     ]
-        // }
 
-        $this->initApi($sessionId);
+        $responseInit = $this->initApi($sessionId);
+
+        $id = $responseInit->getData(true)['SessionID'];
 
         $symptoms = $request->input('symptoms');
 
-        foreach($symptoms as $symptom){
-            $this->addSymptoms($symptom, $sessionId);
+        foreach ($symptoms as $symptom) {
+            $this->addSymptoms($symptom, $id);
         }
 
-        //maybe sessionId is the problem
         $response = Http::get('https://api.endlessmedical.com/v1/dx/Analyze', [
-            'SessionID' => $sessionId,
-            'NumberOfResults' => 5
-        ]);        
-        dd($response);
+            'SessionID' => $id,
+            'NumberOfResults' => 10
+        ]);
+        $data = $response->json();
+
         try {
-            return response()->json(['analyze' => $response]);
-        } catch(\Exception $e) {
+            return response()->json(['analyze' => $data["Diseases"]]);
+        } catch (\Exception $e) {
             return response()->json(['error' => $e->getMessage()]);
         }
-        //analyze when symptoms have been updated
-        //https://api.endlessmedical.com/v1/dx/Analyze?SessionID=$sessionKey&NumberOfResults=5
     }
 
-    public function addSymptoms($symptoms, $sessionId)
+    /**
+     * Updates the session with symptoms provided by the user.
+     * Each symptom is sent to the API to update the session's features.
+     */
+    public function addSymptoms($symptoms, $id)
     {
         foreach ($symptoms as $key => $symptom) {
-            $response = Http::post('https://api.endlessmedical.com/v1/dx/UpdateFeature', [
-                'SessionID' => $sessionId,
-                'name' => $key,
-                'value' => $symptom
-            ]);
+
+            $url = 'https://api.endlessmedical.com/v1/dx/UpdateFeature?SessionID=' . urlencode($id) . '&name=' . urlencode($key) . '&value=' . urlencode($symptom);
+            $response = Http::post($url);
 
             if (!$response->successful()) {
                 return response()->json(['error' => 'Failed to add symptom'], 500);
             }
         }
-
     }
 
-    public function initApi($sessionId){
+    /**
+     * Initializes a session with the medical API and accepts the terms of use.
+     * Returns a new session ID and the status of accepting the terms of use.
+     */
+    public function initApi($sessionId)
+    {
 
         $response = Http::get('https://api.endlessmedical.com/v1/dx/InitSession');
-       
-        $responseInitApi = []; 
+
+        $responseInitApi = [];
 
         if ($response->successful()) {
             $responseData = $response->json();
-            if(isset($responseData['SessionID'])) {
+            if (isset($responseData['SessionID'])) {
                 $sessionId = $responseData['SessionID'];
                 $responseInitApi['SessionID'] = $sessionId;
             }
@@ -215,30 +77,11 @@ class AnalyzeController extends Controller
             $responseInitApi['SessionID'] = 'Failed to init session';
         }
 
-        $responseTerms = Http::post('https://api.endlessmedical.com/v1/dx/AcceptTermsOfUse', [
-            'SessionID' => $sessionId,
-            'passphrase' => urlencode("I have read, understood and I accept and agree to comply with the Terms of Use of EndlessMedicalAPI and Endless Medical services. The Terms of Use are available on endlessmedical.com")
-        ]);
-
-        //error
-        // if ($responseTerms->successful()) {
-        //     $responseDataTerms = $responseTerms->json();
-        //     if (isset($responseDataTerms['status']) && $responseDataTerms['status'] === 'ok') {
-        //         $responseInitApi['Terms'] = $responseDataTerms['status'];
-        //     } else {
-        //         $responseInitApi['Terms'] = 'Error while accepting terms of use';
-        //     }
-        // } else {//entering
-        //     $responseInitApi['Terms'] = 'Failed to request to API';
-        // }
-        
         try {
-            //error here, parameters not appending into url
-            $responseTerms = Http::post('https://api.endlessmedical.com/v1/dx/AcceptTermsOfUse', [
-                'SessionID' => $sessionId,
-                'passphrase' => urlencode("I have read, understood and I accept and agree to comply with the Terms of Use of EndlessMedicalAPI and Endless Medical services. The Terms of Use are available on endlessmedical.com")
-            ]);
-            dd($responseTerms);
+
+            $url = 'https://api.endlessmedical.com/v1/dx/AcceptTermsOfUse?SessionID=' . urlencode($sessionId) . '&passphrase=' . urlencode("I have read, understood and I accept and agree to comply with the Terms of Use of EndlessMedicalAPI and Endless Medical services. The Terms of Use are available on endlessmedical.com");
+            $responseTerms = Http::post($url);
+
             if ($responseTerms->successful()) {
                 $responseDataTerms = $responseTerms->json();
                 if (isset($responseDataTerms['status']) && $responseDataTerms['status'] === 'ok') {
@@ -251,14 +94,11 @@ class AnalyzeController extends Controller
             }
         } catch (RequestException  $e) {
             $errorMessage = $e->getMessage();
-            dd($errorMessage);
         }
-        
 
-        dd($responseInitApi);
-
-        $responseInitApi = response()->json($responseInitApi);
-
+        return response()->json([
+            'SessionID' => $responseInitApi["SessionID"], 
+            'Terms' => $responseInitApi["Terms"]
+        ]);
     }
-
 }
