d5cb831707623a499059c8310c9b2ce76d0c4284
Kevin9SA <kevinsanchezavila@gmail.com>
2024-04-23 19:55:50 +0200

    Integrate chatbot with Laravel (BotMan) #201

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+// lang/en/messages.php
+
+return [
+    'start'=> 'Hi Welcome to Life♥Care say Hello to start',
+    'hello' => 'Hello',
+    'welcome' => 'Hello! What is Your Name?',
+    'error_message'=> 'write Hello for testing...',
+    'saludate' => 'Nice to meet you',
+];
